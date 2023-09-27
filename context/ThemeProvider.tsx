63573f15ada6562e5709cb0b0f9805fcceec5dbf
Alabi Temiloluwa <temiloluwaalabi33@gmail.com>
2023-09-27 19:10:31 +0100

    navbar

--- context/ThemeProvider.tsx
+++ context/ThemeProvider.tsx
@@ -10,18 +10,22 @@ const ThemeContext = createContext<ThemeContextType | undefined>(undefined);
 export function ThemeProvider({ children }: { children: React.ReactNode }) {
   const [mode, setMode] = useState("");
 
-  // const handleThemeChange = () => {
-  //   // if (mode === "dark") {
-  //   //   setMode("light");
-  //   //   document.documentElement.classList.add("light");
-  //   // } else {
-  //   //   setMode("dark");
-  //   //   document.documentElement.classList.add("dark");
-  //   // }
-  // };
-  // useEffect(() => {
-  //   handleThemeChange();
-  // }, [mode]);
+  const handleThemeChange = () => {
+    if (
+      localStorage.theme === "dark" ||
+      (!("theme" in localStorage) &&
+        window.matchMedia("prefers-color-scheme: dark").matches)
+    ) {
+      setMode("dark");
+      document.documentElement.classList.add("dark");
+    } else {
+      setMode("light");
+      document.documentElement.classList.remove("dark");
+    }
+  };
+  useEffect(() => {
+    handleThemeChange();
+  }, [mode]);
 
   return (
     <ThemeContext.Provider value={{ mode, setMode }}>
