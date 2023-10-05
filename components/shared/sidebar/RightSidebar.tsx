b67cfb290b26d0bd11bf9d4e8d2e2c6582ff1e72
Alabi Temiloluwa <temiloluwaalabi33@gmail.com>
2023-10-05 13:14:58 +0100

    homepage done

--- components/shared/sidebar/RightSidebar.tsx
+++ components/shared/sidebar/RightSidebar.tsx
@@ -5,27 +5,27 @@ import RenderTag from "../RenderTag";
 
 const hotQuestions = [
   {
-    _id: 1,
+    _id: "1",
     title:
       "Lorem ipsum dolor sit, amet consectetur adipisicing elit. Vitae quos?",
   },
   {
-    _id: 2,
+    _id: "2",
     title:
       "Lorem ipsum dolor sit, amet consectetur adipisicing elit. Vitae quos?",
   },
   {
-    _id: 3,
+    _id: "3",
     title:
       "Lorem ipsum dolor sit, amet consectetur adipisicing elit. Vitae quos?",
   },
   {
-    _id: 4,
+    _id: "4",
     title:
       "Lorem ipsum dolor sit, amet consectetur adipisicing elit. Vitae quos?",
   },
   {
-    _id: 5,
+    _id: "5",
     title:
       "Lorem ipsum dolor sit, amet consectetur adipisicing elit. Vitae quos?",
   },
@@ -33,31 +33,31 @@ const hotQuestions = [
 
 const popularTags = [
   {
-  _id: 1,
-  name: 'javascript',
-  totlaQuestions: 5
+    _id: "1",
+    name: "javascript",
+    totlaQuestions: 5,
   },
   {
-  _id: 2,
-  name: 'html',
-  totlaQuestions: 5
+    _id: "2",
+    name: "html",
+    totlaQuestions: 5,
   },
   {
-  _id: 3,
-  name: 'css',
-  totlaQuestions: 5
+    _id: "3",
+    name: "css",
+    totlaQuestions: 5,
   },
   {
-  _id: 4,
-  name: 'php',
-  totlaQuestions: 5
+    _id: "4",
+    name: "php",
+    totlaQuestions: 5,
   },
   {
-  _id: 5,
-  name: 'python',
-  totlaQuestions: 5
+    _id: "5",
+    name: "python",
+    totlaQuestions: 5,
   },
-]
+];
 const RightSidebar = () => {
   return (
     <section className="background-light900_dark200 light-border custom-scrollbar sticky right-0 top-0 flex h-screen w-[350px] flex-col gap-5 overflow-y-auto border-l p-6 pt-36 shadow-light-300 dark:shadow-none max-xl:hidden">
@@ -85,17 +85,15 @@ const RightSidebar = () => {
       <div className="mt-16 ">
         <h3 className="h3-bold text-dark200_light900">Popular Tags</h3>
         <div className="mt-7 flex flex-col gap-4">
-          {
-            popularTags.map((tag) => (
-                <RenderTag 
-                  key={tag._id}
-                  _id={tag._id}
-                  name={tag.name}
-                  totalQuestions={tag.totlaQuestions}
-                  showCount
-                />
-            ))
-          }    
+          {popularTags.map((tag) => (
+            <RenderTag
+              key={tag._id}
+              _id={tag._id}
+              name={tag.name}
+              totalQuestions={tag.totlaQuestions}
+              showCount
+            />
+          ))}
         </div>
       </div>
     </section>
