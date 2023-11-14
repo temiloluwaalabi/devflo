7d7219286e119230c7b07c51a66e770cd3bf84bd
Alabi Temiloluwa <temiloluwaalabi33@gmail.com>
2023-11-14 19:54:05 +0100

    afterAuth middleware

--- middleware.ts
+++ middleware.ts
@@ -1,16 +1,21 @@
-import { authMiddleware } from "@clerk/nextjs";
+import { authMiddleware, redirectToSignIn } from "@clerk/nextjs";
  
 export default authMiddleware({
   publicRoutes: [
     '/',
     '/api/webhook',
-    '/question/:id',
+    // '/question/:id',
     '/tags',
     '/tags/:id',
     '/profile/:id',
     '/community',
     '/jobs'
   ],
+  afterAuth(auth, req, evt){
+    if(!auth.userId && !auth.isPublicRoute){
+      return redirectToSignIn({returnBackUrl: req.url})
+    }
+  },
   ignoredRoutes:[
     '/api/webhook', '/api/chatgpt'
   ]
